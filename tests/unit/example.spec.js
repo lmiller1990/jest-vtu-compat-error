9866923551b5d541e3110562633e626666d71195
Lachlan Miller <lachlan.miller.1990@outlook.com>
2022-08-11 15:50:39 +1000

    repro

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,43 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+const Vue = require('vue')
+const { ref, onMounted, defineComponent, h, configureCompat } = Vue
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      props: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
-  })
-})
+configureCompat({ MODE: 2, })
+
+const VueTestUtils = require("@vue/test-utils")
+const { mount, flushPromises } = VueTestUtils
+const { render } = require("@testing-library/vue")
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+const TestAsync = defineComponent({
+  setup() {
+    const mountText = ref();
+    const asyncText = ref();
+
+    onMounted(() => {
+      mountText.value = "mounted";
+    });
+
+    sleep(0).then(() => {
+      asyncText.value = "async";
+    });
+
+    return () => h('div', `${mountText.value} and ${asyncText.value}`)
+  },
+});
+
+it.only("should show onMount text", async () => {
+  const { findByText, queryByText } = render(TestAsync);
+  await flushPromises()
+  console.log(document.body.outerHTML)
+  await queryByText('mounted')
+
+  if (!document.body.outerHTML.includes('mounted')) {
+    throw Error('Uh oh')
+  } else {
+    console.log('Works great!')
+  }
+});
+
